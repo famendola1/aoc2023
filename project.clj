d2c1dff98843c224ac50940207cb27490db4e282
Fabio Amendola <fabio.amendola20@gmail.com>
2023-12-03 12:08:44 -0500

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject advent-of-code "0.1.0"
-  :description "Basis/template for Advent of Code"
-  :url "https://github.com/rjray/advent-clojure-basis"
+  :description "Solutions for Advent of Code 2023"
+  :url "https://github.com/famendola1/aoc2023"
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.0"]
